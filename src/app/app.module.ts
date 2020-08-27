8b33e198dcd1b601ee0b87659b3d3264feb9770e
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-08-27 10:25:32 +0700

    Add global style and login/signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginAndSignupComponent } from './login-and-signup/login-and-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginAndSignupComponent
   ],
   imports: [
     BrowserModule,

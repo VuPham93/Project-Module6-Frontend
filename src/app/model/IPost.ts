a0a8e43e3e536281456771c958be3dc49576685b
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-08-31 13:37:14 +0700

    add post status

---
+++ src/app/model/IPost.ts
@@ -0,0 +1,13 @@
+export interface IPost {
+  postId: number,
+  posterId: number,
+  posterAvatar: string,
+  posterName: string,
+  textPost: string,
+  imagePost: string,
+  videoPost: string,
+  linkPost: string,
+  postTime: string,
+  postLike: number,
+  postDislike: number
+}

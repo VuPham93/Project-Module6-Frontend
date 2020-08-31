f2588e4f73aa542042c506b3c119d1dc742ffe5e
hungviet276 <viethungkt7k6@gmail.com>
2020-08-31 15:49:14 +0700

    add button add friend

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {IUser} from '../model/iuser';
+import {UserService} from '../service/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  user: IUser;
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.setUser();
+  }
+  setUser() {
+    this.userService.getUser().subscribe(
+      response => {this.user = <IUser> response;
+      },
+      error => console.error(error)
+    );
   }
 
 }
